650685e567bfbbc7a381213add282ffd08f71fce
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2021-01-05 21:11:22 -0700

    workout schema created

--- models/model.js
+++ models/model.js
@@ -1,3 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const exerciseSchema = new Schema({
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Choose cardio or resistance"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter name of exercise"
+        },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Duration of exercise"
+        },
+        weight: {
+            type: Number,
+            trim: true,
+            required: "Weight used for exercise"
+        },
+        reps: {
+            type: Number,
+            trim: true,
+            required: "Enter repitition of the exercise"
+        },
+        sets: {
+            type: Number,
+            trim: true,
+            required: "Enter amount of sets"
+        },
+    }]
+})
+
+module.exports = exerciseSchema;
