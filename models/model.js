362d43411c0760d53d0a7575b6154d9215d4c841
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2021-01-05 21:13:09 -0700

    added day to workout schema

--- models/model.js
+++ models/model.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
     exercises: [{
         type: {
             type: String,
